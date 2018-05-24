648abde9a201fb261969b05d13344f0b901e9c26
xrust <rust8013@gmail.com>
2018-05-24 23:30:16 +0400

    second merge commit

--- src/TryCommiteAndPush.java
+++ src/TryCommiteAndPush.java
@@ -1,6 +1,6 @@
 public class TryCommiteAndPush {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world one");
         System.out.println("Hello world");
     }
 }
