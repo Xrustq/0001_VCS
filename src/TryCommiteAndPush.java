2c7ea60318c0074692483f06c59e4904a8e2512b
xrust <rust8013@gmail.com>
2018-05-24 23:39:21 +0400

    fifthCommit

--- src/TryCommiteAndPush.java
+++ src/TryCommiteAndPush.java
@@ -4,5 +4,6 @@ public class TryCommiteAndPush {
         System.out.println("Hello world");
         System.out.println("Hello world");
         System.out.println("Hello world");
+        System.out.println("Hello world");
     }
 }
