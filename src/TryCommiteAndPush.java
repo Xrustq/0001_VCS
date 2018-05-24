9aded10a2ed4fcc38c78eaedab3a56c37f17220f
xrust <rust8013@gmail.com>
2018-05-24 23:55:55 +0400

    commit in request branch

--- src/TryCommiteAndPush.java
+++ src/TryCommiteAndPush.java
@@ -6,5 +6,7 @@ public class TryCommiteAndPush {
         System.out.println("Hello world");
         System.out.println("Hello world");
         System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
     }
 }
